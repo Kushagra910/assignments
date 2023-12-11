7dfe8886bb187a93f98ac005637fdc496fdd4afa
Kushagra910 <kushagram63@gmail.com>
2023-12-11 17:05:36 +0530

    solved anangram prob

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,27 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let temp1 = str1.toLowerCase();
+  let temp2 = str2.toLowerCase();
+  let arr1 = [];
+  let arr2 = [];
+  for (let i = 0; i < temp1.length; i++) {
+    arr1.push(temp1[i]);
+  }
+  for (let i = 0; i < temp2.length; i++) {
+    arr2.push(temp2[i]);
+  }
+  let s1 = "";
+  let s2 = "";
+  arr1.sort();
+  arr2.sort();
+  for (let i = 0; i < arr1.length; i++) {
+    s1 += arr1[i];
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    s2 += arr2[i];
+  }
+  return s1 == s2;
 }
 
 module.exports = isAnagram;
